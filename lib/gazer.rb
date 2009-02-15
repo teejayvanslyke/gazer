9e179040a3971cf744393262305a9f32184f1bae
T.J. VanSlyke <tj@elctech.com>
2009-02-14 21:24:51 -0800

    added after for class methods

--- lib/gazer.rb
+++ lib/gazer.rb
@@ -8,7 +8,6 @@ module Gazer
     block.call
   end
 
-
   Object.instance_eval do 
     class << self
 
@@ -18,44 +17,76 @@ module Gazer
         @advice[sym] << block
       end
 
-      def advice(sym)
+      def advice
+        @advice ||= {}
+      end
+
+      def advice_for(sym)
         @advice[sym]
       end
+
     end
 
     def self.advise_before(sym, &block)
       advise(sym, block)
-
-      str_id = "__#{sym}__hooked__"
-      unless private_instance_methods.include?(str_id)
-        code = <<-CODE
-          class << self
-            alias_method #{str_id.inspect}, #{sym.inspect}  
-            private #{str_id.inspect}
-            define_method #{sym.inspect} do |*args|    
-              self.advice(#{sym.inspect}).first.call(:object => self,              # Invoke hook
-                         :method => #{sym.inspect}, 
-                         :args => args
-                        )
-              __send__ #{str_id.inspect}, *args  # Invoke backup
-            end
+      code = <<-CODE
+        class << self
+          hook = backup_method(#{sym.inspect})
+          define_method #{sym.inspect} do |*args|    
+            self.advice_for(#{sym.inspect}).first.call(:object => self,              # Invoke hook
+                       :method => #{sym.inspect}, 
+                       :args => args
+                      )
+            __send__ hook, *args  # Invoke backup
           end
-        CODE
-        instance_eval(code)
-      end
+        end
+      CODE
+      instance_eval(code)
     end
 
     def self.advise_instances_before(sym, &block)
-      str_id = "__#{sym}__hooked__"
-      unless private_instance_methods.include?(str_id)
-        alias_method str_id, sym        # Backup original method
-        private str_id                  # Make backup private
+      hook = backup_method(sym)
+      define_method sym do |*args|    # Replace method
+        block.call(:object => self,              # Invoke hook
+                   :method => sym, 
+                   :args => args
+                  )
+        __send__ hook, *args  # Invoke backup
+      end
+    end
+
+
+    def self.advise_instances_after(sym, &block)
+      hook = backup_method(sym)
+      define_method sym do |*args|    # Replace method
+        rval = __send__ hook, *args  # Invoke backup
+        block.call(:object => self,              # Invoke hook
+                   :method => sym, 
+                   :args => args
+                  )
+        return rval
+      end
+    end
+
+    def backup_method(sym)
+      id = "__#{sym}_#{backup_methods_for(sym).size}__"
+      alias_method id, sym        # Backup original method
+      private id                  # Make backup private
+      @backup_methods_for ||= {}
+      @backup_methods_for[sym] ||= []
+      @backup_methods_for[sym] << id
+      return id
+    end
+
+    def backup_methods_for(sym)
+      @backup_methods_for ||= {}
+      @backup_methods_for[sym] ||= []
+    end
+
+    def unadvise_all
+      advice.each do |sym, arr|
         define_method sym do |*args|    # Replace method
-          block.call(:object => self,              # Invoke hook
-                     :method => sym, 
-                     :args => args
-                    )
-          __send__ str_id, *args  # Invoke backup
+          __send__ backup_methods_for(sym).first, *args  # Invoke backup
         end
       end
     end
--- spec/gazer_spec.rb
+++ spec/gazer_spec.rb
@@ -3,6 +3,7 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 def message(text) end
 describe Gazer do
+
   include Gazer
 
   class Dog
@@ -21,20 +22,16 @@ describe Gazer do
     def hiss!
     end
 
+    def run_away!
+    end
+
     def self.get_scared
     end
   end
 
-  Gazer::Aspect :cat_fight do
-
-    Dog.advise_instances_before(:bark!) do |point|
-      Cat.new.hiss!
-    end
-
-    Dog.advise_before(:create) do |point|
-      Cat.get_scared
-    end
-
+  after :each do
+    Dog.unadvise_all
+    Cat.unadvise_all
   end
 
   describe "advising instance methods" do
@@ -44,13 +41,32 @@ describe Gazer do
       it "executes the advice provided before the method is executed" do
 
         @cat = Cat.new
+
+        Dog.advise_instances_before(:bark!) do |point|
+          @cat.hiss!
+        end
+
         @dog = Dog.new
-
-        Cat.should_receive(:new).and_return(@cat)
         @cat.should_receive(:hiss!)
-
         @dog.bark! 
 
+      end
+
+    end
+
+    describe "after the method call" do
+
+      it "executes the advice provided after the method is executed" do
+
+        @cat = Cat.new
+
+        Dog.advise_instances_after(:bark!) do |point|
+          @cat.run_away!
+        end
+
+        @dog = Dog.new
+        @cat.should_receive(:run_away!)
+        @dog.bark! 
       end
 
     end
@@ -62,6 +78,10 @@ describe Gazer do
     describe "before the method call" do
 
       it "executes the advice provided before the method is executed" do
+
+        Dog.advise_before(:create) do |point|
+          Cat.get_scared
+        end
 
         Cat.should_receive(:get_scared)
         @dog = Dog.create
